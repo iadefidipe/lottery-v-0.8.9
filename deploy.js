7e4e56004c0501121cbaf25da232dee63e26daf8
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-12 15:30:50 +0100

    feat: fixed deploy bugs

--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ const { abi, evm } = require('./compile')
 
 const provider = new HDWalletProvider(
 	'account mnemonic',
-	'https://rinkeby.infura.io/v3/cfde4e8318514308bbad06c2fd4681ed'
+	'infura link'
 )
 
 const web3 = new Web3(provider) //creating a new instance of web3
@@ -12,19 +12,17 @@ const web3 = new Web3(provider) //creating a new instance of web3
 const deploy = async () => {
 	const accounts = await web3.eth.getAccounts()
 
-	console.log('Attempting to deploy from Account', accounts[0])
+	console.log('Attempting to deploy from Account', accounts[1])
 
-	const contract = await new web3.eth.Contract(JSON.parse(abi)) //creating contract instance
+	const contract = await new web3.eth.Contract(abi) //creating contract instance
 		.deploy({
-			data: evm.bytecode.object,
-			arguments: ['GM!'],
+			data: evm.bytecode.object
 		})
-		.send({ gas: '1000000', from: accounts[0] })
+		.send({ gas: '1000000', from: accounts[1] })
 
 	console.log('contract deployed to ', contract.options.address)
+	console.log('ABI', abi)
+
 	provider.engine.stop()
 }
 deploy()
-// https://rinkeby.infura.io/v3/cfde4e8318514308bbad06c2fd4681ed
-// 0x436475159886f54686887c9c6Ba30EC396d02fDd
-// contract address: 0xEdBC95b4ef0bC8d21523012472A36Ee69a4b2F14
